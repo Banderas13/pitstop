8004dd655a603e1bf39da105b7124c44f45459f4
Seth_Poelmans <seth.poelmans@gmail.com>
2025-06-08 16:18:11 +0200

    Routes enzo

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,11 @@ use App\Http\Controllers\DashboardController;
 
 Route::get('/', [DashboardController::class, 'index']);
 
+Route::get('/uploads', [DashboardController::class, 'index'])->middleware('auth');
 
+Route::get('/login', function () {
+    return redirect('/admin/login');
+})->name('login');
 
 Route::post('/logout', function () {
     Auth::logout();
