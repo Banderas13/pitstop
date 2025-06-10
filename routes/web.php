7366a58fda29dd83942be144cd108ad5099eac59
Julio Raynier Castro Vargas <raynier97@hotmail.com>
2025-06-10 14:15:44 +0200

    Add car routes with authentication: include resource routes and search functionality

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,17 @@ use App\Http\Middleware\IsAdmin;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CarController;
 
 
 Route::get('/', [DashboardController::class, 'index']);
 
-
+// Car routes (require authentication)
+Route::middleware('auth')->group(function () {
+    Route::resource('cars', CarController::class)->except(['show', 'edit', 'update']);
+    Route::get('search-brands', [CarController::class, 'searchBrands'])->name('cars.searchBrands');
+    Route::get('search-models', [CarController::class, 'searchModels'])->name('cars.searchModels');
+});
 
 Route::post('/logout', function () {
     Auth::logout();
